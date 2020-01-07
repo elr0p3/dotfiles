1956851db8db3beb2d8d696e6841becdabab9884
r0p3 <33119383+ropefarlopa@users.noreply.github.com>
2020-01-07 21:29:14 +0100

    Create launch.sh

---
+++ home/r0p3/.config/polybar/launch.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+# polybar example &
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar bar1 >>/tmp/polybar1.log 2>&1 &
+# polybar bar2 >>/tmp/polybar2.log 2>&1 &
+polybar r0p3-bar -r &
+
+echo "Polybar launched..."
